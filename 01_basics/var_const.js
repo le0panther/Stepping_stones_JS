2bb12020800157fac0b66b3becc512bbfc42df9e
Leo_Panther <pantabhishek202@gmail.com>
2026-04-08 16:52:57 +0000

    Add changes

---
+++ 01_basics/var_const.js
@@ -0,0 +1,9 @@
+const uuid=4562231;
+let employer_name="Abhishek"
+var employer_address="Jagdalpur"
+
+employer_age=20
+
+//always use let instead of var as it has scope of resolution and the 'var' keyword don't
+
+console.table([uuid,employer_address,employer_name,employer_age])
