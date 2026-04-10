fbd8f48a098f8091be8cada6d970f0039bb27d75
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-10 23:40:15 +0530

    Add Changes

--- 01_basics/num_math.js
+++ 01_basics/num_math.js
@@ -5,6 +5,8 @@ const other_Number=new Number(4000)
 console.log(other_Number)       
 console.log(typeof other_Number)
 
+console.log(String(other_Number).length)    //converted the type of the data'other_number' and applied the operations of the data-type'String' to which it is converted .
+
 //as using this new method alocates the data into heap rather then stack making the process slow by increasing the operations like garbage collection etc.
 
 //Hence , using 'new' keyword to initialize a primitive data-type is considered a bad practise.
