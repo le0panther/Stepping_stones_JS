5748247e1965f1182371d1ccca45166224741e35
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-10 17:17:56 +0530

    Add Changes

---
+++ 01_basics/num_math.js
@@ -0,0 +1,10 @@
+const some_Number=400;  // always try to use the primitive method to initialize the imperative type 
+console.log(some_Number)
+
+const other_Number=new Number(4000)
+console.log(other_Number)       
+console.log(typeof other_Number)
+
+//as using this new method alocates the data into heap rather then stack making the process slow by increasing the operations like garbage collection etc.
+
+//Hence , using 'new' keyword to initialize a primitive data-type is considered a bad practise.
