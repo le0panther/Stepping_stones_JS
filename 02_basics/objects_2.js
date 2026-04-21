e96e5d25a10a88cb92d8c09d0aa96d2cfdf581ca
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-21 15:57:01 +0530

    Add Changes

--- 02_basics/objects_2.js
+++ 02_basics/objects_2.js
@@ -10,6 +10,8 @@ object_1.Gender="Male"
 object_1.Age=20
 
 //console.log(object_1)
+//console.log(Object.keys(object_1))      //Prints the keys of the object "The return type is an array"
+//console.log(Object.values(object_1))    //Prints the values of the objects "The return type is an array"
 
 //Entering an another object inside the object_1 and and accessing it's values
 
@@ -22,13 +24,30 @@ const object3={
 }
 
 //accessing values form the object3 
-console.log(object3.name.Last_name)     //Prints Specifically the Last name
-console.log(object3.name)               //Prints the entire object inside the name
+//console.log(object3.name.Last_name)     //Prints Specifically the Last name
+//console.log(object3.name)               //Prints the entire object inside the name
 
-//Combining two objects just like we did in array's
+//Combining two objects just like we did in array's using "Spread Operator "
 
 const obj_2={1:"a",2:"B"}
-const obj_2={3:"C",4:"D"}
+const obj_3={3:"C",4:"D"}
+
+//Combines them without creating a new object (Not used widely only in particular cases)
+// Object.assign(obj_2,obj_3)
+// console.log(obj_2)
+
+//mehtod 2 for doing the same (Widely used in the Modern web Development)
+const newObject={...obj_2,...obj_3}     
+//this method combines them together but the reading is not easy here and creates a new object 
+console.log(newObject)
+
+//Reading the object created in the method 2 is quite difficult so making it a bit simpler
+
+
+
+
+
+
 
 
 
