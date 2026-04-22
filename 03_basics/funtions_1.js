a748f7e6137d3ceef4cd3f06c5fae7cae10decac
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-22 12:00:34 +0530

    Basics of Funtions

---
+++ 03_basics/funtions_1.js
@@ -0,0 +1,13 @@
+//Syntax of a funtion 
+
+// funtion "AnyName"(){
+    
+// }
+
+function add (){        //eg of a funtion 
+    const num1=90
+    const num2=10
+    console.log(num1+num2)
+}
+
+add()       //Calling the funtion 
