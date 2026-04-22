a91d52436e598f3d80cc257ecc4f65101e5530dd
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-22 12:15:35 +0530

    Basics of Funtions

--- 03_basics/funtions_1.js
+++ 03_basics/funtions_1.js
@@ -7,7 +7,16 @@
 function add (p1,p2){        //eg of a funtion 
     const num1=p1
     const num2=p2
-    console.log(num1+num2)
+    //console.log(num1+num2)  
+    return num1+num2  
+    console.log("someNmae")     
+    //This line of code won't be getting executed as return ends the funtion and return the value where it is being called       
 }
 
-add(90,10)       //Calling the funtion 
+// add(90,10) 
+// //Calling the funtion ,if the user won't be giving a number then a issue might come as NaN as no number have been provided to it 
+
+const val=add(90,10)
+console.log(val)        
+//This line of code will result in giving us undefined as the console.log dont return the value , but when we use "return" in place of console.lod inside the funtion it will result in printing the vlaue 
+
