9de98a067a2729ff9de39ffbf2c64a9dc760bbd7
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-25 19:25:52 +0530

    This and Arrow Funtion

--- 03_basics/This_And_Arrow_Funtion.js
+++ 03_basics/This_And_Arrow_Funtion.js
@@ -5,6 +5,8 @@ const obj1={
     Price:199,
     welcome:function () {
         console.log(`Welcome ${this.name} to the server .`)
+        console.log(this) 
+        //here this will be giving result based on current context which is here console.log(this) is equal to console.log(obj1)
     }
 }
 //jab hum current context ko refer karte hai tab hum "this" keyword ka use karte hai ,meaning trying to refer or use the value in the current scope .
@@ -12,3 +14,8 @@ const obj1={
 console.log(obj1.welcome())
 
 //But when this is written globally , i mean when this is not inside any particular scope but as a global scopt then what will be happening.
+
+console.log(this)
+//here we will be receving an empty object as their is no global scope object inside the node.js enviroment 
+
+//But in browser their will be windows object and hence it will not be empty , also this window object is reason why we are able to perform various events on the browser .
