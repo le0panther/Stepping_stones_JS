1c32d8f4c538f6c12c377e8f5cce9564bb085312
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-25 19:14:55 +0530

    This and Arrow Funtion

--- 03_basics/This_And_Arrow_Funtion.js
+++ 03_basics/This_And_Arrow_Funtion.js
@@ -1,9 +1,14 @@
 //This file will be containing the notes that helped me learn " this and arrow funtion" 
 
-const obj1={
+const obj1={        
     name:"Abhishek Pant",
     Price:199,
     welcome:function () {
-        console.log(`Welcome ${name} to the server .`)
+        console.log(`Welcome ${this.name} to the server .`)
     }
 }
+//jab hum current context ko refer karte hai tab hum "this" keyword ka use karte hai ,meaning trying to refer or use the value in the current scope .
+
+console.log(obj1.welcome())
+
+//But when this is written globally , i mean when this is not inside any particular scope but as a global scopt then what will be happening.
