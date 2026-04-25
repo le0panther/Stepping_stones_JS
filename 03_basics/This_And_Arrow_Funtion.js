88ffa638b471e103ad56260eb1395d7ed8ea5467
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-25 19:05:02 +0530

    This and Arrow Funtion

--- 03_basics/This_And_Arrow_Funtion.js
+++ 03_basics/This_And_Arrow_Funtion.js
@@ -1,1 +1,9 @@
 //This file will be containing the notes that helped me learn " this and arrow funtion" 
+
+const obj1={
+    name:"Abhishek Pant",
+    Price:199,
+    welcome:function () {
+        console.log(`Welcome ${name} to the server .`)
+    }
+}
