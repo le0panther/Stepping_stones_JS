fd237251e4590e186624ff868e336c145e7ae97c
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-26 16:36:05 +0530

    IIFE

--- 03_basics/iife.js
+++ 03_basics/iife.js
@@ -1,11 +1,28 @@
 //The file  and code which helped me lear IIFE
 
+// IIFE is widely used for two main resons for :-
+// 1)For protecting the variables inside the funtion scope to from getting poluted form the global values and vice-versa,(it protects it form name collesion )
+// 2)for running the function  immeditely after its declaration 
+
+
 //Syntax = () ()
 // (here we write the funtion defination )
 // (Another paranthesis is their which is used for its immediate execution )
 
-function message(){
-    console.log("hello user ")
-}
+// function message(){
+//     console.log("hello user ")
+// }
+//The above function in IIFE form 
 
-message()
+(function message(){
+    console.log("hello user ")
+})()
+
+
+// (()=>{
+//     console.log("hello bro")
+// })()  
+//Immediately executing a arrow funtion without the use of variables
+
+
+// message()
