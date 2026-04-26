7b0b8f5e118cfdedc7c2ea6abba69b568b4d9e8a
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-26 16:26:46 +0530

    IIFE

---
+++ 03_basics/iife.js
@@ -0,0 +1,11 @@
+//The file  and code which helped me lear IIFE
+
+//Syntax = () ()
+// (here we write the funtion defination )
+// (Another paranthesis is their which is used for its immediate execution )
+
+function message(){
+    console.log("hello user ")
+}
+
+message()
