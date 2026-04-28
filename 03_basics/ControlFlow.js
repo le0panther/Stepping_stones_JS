84899e9082c0157e3c3d33cdf79d446101dccd3c
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-29 01:05:03 +0530

    Control Flow / Logic Control

--- 03_basics/ControlFlow.js
+++ 03_basics/ControlFlow.js
@@ -2,15 +2,37 @@
 
 // if(true){       //syntax 
 
-// } else-if(){
+// } else if(true){
 
 // }
 
-// else{       sytnax 
+// else{      
 
 // }
 
 //shorthand notation 
 
 let balance =1000
-if(balance<10000) console.log("Its an example of ShortHand Notaion ");  //it ends with a semicolon 
+if(balance<10000) console.log("Its an example of ShortHand Notaion ");  //it ends with a semicolon and for single line code 
+
+
+//switch case
+
+// switch(key){
+//     case value:
+//         break;
+
+//     casevalue_2:
+//         break;
+
+// }
+
+
+switch(balance){
+    case 300:
+        break;
+    
+    case 1000:
+        console.log("Switch-Case")
+        break;
+}
