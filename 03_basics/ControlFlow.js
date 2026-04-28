d9e760d31e2f527fa76318f7f9c0219fc36eedc8
Abhishek Pant <pantabhishek202@gmail.com>
2026-04-29 00:38:14 +0530

    Control Flow / Logic Control

--- 03_basics/ControlFlow.js
+++ 03_basics/ControlFlow.js
@@ -1,1 +1,16 @@
-// The codes which helped me learned Control Flow in javascript 
+// The codes which helped me learned Control Flow (Conditional Statements)
+
+// if(true){       //syntax 
+
+// } else-if(){
+
+// }
+
+// else{       sytnax 
+
+// }
+
+//shorthand notation 
+
+let balance =1000
+if(balance<10000) console.log("Its an example of ShortHand Notaion ");  //it ends with a semicolon 
