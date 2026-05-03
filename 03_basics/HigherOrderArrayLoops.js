b793bda2faaa193701fa5a603f1dc5a1e1df5b7b
Abhishek Pant <pantabhishek202@gmail.com>
2026-05-03 22:25:15 +0530

    Higher Order Array Loops

---
+++ 03_basics/HigherOrderArrayLoops.js
@@ -0,0 +1,19 @@
+//for of 
+
+let array=[1,2,3,6,5,4,5,6,5,4]
+
+for (const arr of array) {
+    console.log(arr)
+    
+}
+
+const string="Hello World "
+for (const str of string) {
+    if(str===" "){
+        continue
+    }
+    console.log(str)
+    
+}
+
+//jada kuch nahi for of is used to print the entire value without the need for declaration of any variable and increment operator in the block of the  for 
