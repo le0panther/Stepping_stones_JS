bd4ba26ab81744f2ea4bc427e4eebcc5a48f3995
Abhishek Pant <pantabhishek202@gmail.com>
2026-05-03 20:11:02 +0530

    Iterators

--- 03_basics/Loops.js
+++ 03_basics/Loops.js
@@ -1,3 +1,14 @@
 for( let i=0;i<=10;i++){
     console.log(i)
 }
+
+//break and continue 
+//break will get you out of the scope and end the iteration , while control on the other hand will only skip a single iteration 
+
+const array=["Super-Man","Batman","Spider man"]
+for(let index=0;index<array.length;index++){
+    if(index===2){
+        break
+    }
+    console.log(`${array[index]}`)
+}
