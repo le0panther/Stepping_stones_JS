d8f8b705cb92ffdf5454a3d34b4eb50e2af36a3f
Abhishek Pant <pantabhishek202@gmail.com>
2026-05-04 15:31:15 +0530

    Filter Map and Reduce

--- 03_basics/FilterMapAndReduce.js
+++ 03_basics/FilterMapAndReduce.js
@@ -1,12 +1,13 @@
-let array=[1,2,3,6,5,4,5,6,5,4]
+let array=[1,2,3,4]
 
 array.forEach((item)=>{     //simply using it to print the values
-    console.log(item)
+   // console.log(item)
 })
 
 const val=array.forEach((item)=>{     //storing and printing the val itself to see what will the forEach value return i
     console.log(item)
 })
 
-console.log(val)
+console.log(val)        
+//it will return an undefined at the end as forEach can't be used to return any values as its return values is always undefined 
 
