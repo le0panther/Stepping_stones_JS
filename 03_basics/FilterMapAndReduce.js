eba2451fe21a0ff85757c7f02ae9c8833df18323
Abhishek Pant <pantabhishek202@gmail.com>
2026-05-04 15:29:14 +0530

    Filter Map and Reduce

---
+++ 03_basics/FilterMapAndReduce.js
@@ -0,0 +1,12 @@
+let array=[1,2,3,6,5,4,5,6,5,4]
+
+array.forEach((item)=>{     //simply using it to print the values
+    console.log(item)
+})
+
+const val=array.forEach((item)=>{     //storing and printing the val itself to see what will the forEach value return i
+    console.log(item)
+})
+
+console.log(val)
+
