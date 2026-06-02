d525f712d5195fa92663199bd147871f3a9df94c
Abhishek Pant <pantabhishek202@gmail.com>
2026-06-03 01:09:00 +0530

    Events Handling

---
+++ 05_Events/three.js
@@ -0,0 +1,15 @@
+let setInterval_1;
+const changeColor = function () {
+  const Rand = (Math.random() * 1000000).toFixed();
+  console.log(`Changing the color to ${Rand}`)
+  document.querySelector('body').style.backgroundColor =`#${Rand}` ;
+};
+
+document.querySelector('#start').addEventListener('click', function () {
+  setInterval_1 = setInterval(changeColor, 2000);
+});
+
+document.querySelector('#stop').addEventListener('click', function () {
+  clearInterval(setInterval_1);
+  console.log("The Color had been choosen ")
+});
